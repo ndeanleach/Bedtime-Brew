feaf2292abfe1df1164ec7f260ef5bfad8e8a871
deannate00 <deannate00@gmail.com>
2023-09-27 18:11:47 -0400

    Initial Commit

---
+++ Bedtime Brew/Bedtime_BrewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bedtime_BrewApp.swift
+//  Bedtime Brew
+//
+//  Created by Nathan Leach on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Bedtime_BrewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
